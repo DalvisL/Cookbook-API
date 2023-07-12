861b194014c36c7b005b5e90eece7fc3307e810e
Dalvis <Dalvis.liz27@gmail.com>
2023-07-12 09:32:14 -0400

    fixed router endpoint and added a helper function

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 
-// const Recipes = require('./recipes-model.js');
+const Recipes = require('./recipes-model.js');
 
-router.get('*', (req, res) => {
+router.get('/:recipe_id', async (req, res, next) => {
+    try {
+        const recipe = await Recipes.getRecipeById(req.params.recipe_id) // eslint-disable-line
+            .then(recipe => {
+                if (recipe === null) {
+                    res.status(404).json({
+                        message: 'Recipe not found',
+                    });
+                } else {
+                    res.json(recipe);
+                }
+            })
+            .catch(next);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('*', (req, res, next) => { // eslint-disable-line
     res.json({
         message: 'Welcome to the Recipes API',
     });
